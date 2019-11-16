8a082fc4151f0e40b19ade33af28df372a9362ca
Dmitry Vdovichenko <kenar89@gmail.com>
2019-11-16 15:37:30 +0500

    styling form scss modules added

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -2,32 +2,38 @@ import React from 'react'
 import propTypes from 'prop-types'
 import { User } from 'react-feather';
 import {store} from '../store'
+import styles from './SignIn.module.scss';
 
 let input
 const SignIn = (props) => (
     
-    <div>
-        <label htmlFor="user">Enter an username</label>
-        <input 
-            
-            id="user"
-            onKeyPress={(e) => {
-                if (e.key === 'Enter'){
-                    console.log(props)
-                    props.dispatch(input.value)
-                    console.log(store.getState())
-                    props.history.push( {pathname:'/chat', state:store.getState()})
-                    input.value = ''
-                    
-                }
-            }}
-            type="text"
-            ref={(node) =>{
-                input = node
-            }}
-        >
+    <div className={styles.signIn}>
+        <h1 className={styles.signIn__header}> Enter to our friendly chat</h1>
+        <div className={styles.signIn__form}>
+            <input 
+                className={styles.signIn__input}
+                id="user"
+                onKeyPress={(e) => {
+                    if (e.key === 'Enter'){
+                        console.log(props)
+                        props.dispatch(input.value)
+                        console.log(store.getState())
+                        props.history.push( {pathname:'/chat', state:store.getState()})
+                        input.value = ''
+                        
+                    }
+                }}
+                placeholder="Enter your username"
+                type="text"
+                ref={(node) =>{
+                    input = node
+                }}
+            >
 
-        </input>
+            </input>
+        </div>
+        
+
         </div>
     
 )
